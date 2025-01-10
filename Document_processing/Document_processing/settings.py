e83f3321789b508b92656c05bcb8b21be7e3a621
Mina Markos <minalxr123@gmail.com>
2025-01-10 02:31:56 +0200

    trying after deploying 2

--- Document_processing/Document_processing/settings.py
+++ Document_processing/Document_processing/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-v8^bzpr!u$-xdg&ibwjts!jvmcb(xj==lrs@2(j@5@7w271t%w'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['MinaMarkos.pythonanywhere.com']
 
 
 # Application definition
